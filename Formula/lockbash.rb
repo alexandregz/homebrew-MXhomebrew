06fd5e6f1a2646da9387569a1c0fd9505d68df3f
Alexandre Espinosa Menor <aemenor@gmail.com>
2015-02-18 10:20:50 +0100

    Create lockbash.rb

---
+++ Formula/lockbash.rb
@@ -0,0 +1,16 @@
+class Lockbash < Formula
+  homepage "https://github.com/alexandregz/lockbash.git"
+  url "https://github.com/alexandregz/lockbash.git"
+  sha1 "3452888e7f01c7cbcf8e454cdea0f6ee78105d85"
+  version "1.0"
+
+  depends_on "bash" => :optional
+
+  def install
+    bin.install "lockbash"
+  end
+
+  test do
+    assert_match "Usage:", shell_output("#{bin}/lockbash")
+  end
+end
