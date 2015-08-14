d384b7e9b18ad3b029ced1e26db17d2f8629bca5
Alexandre Espinosa Menor <aemenor@gmail.com>
2015-08-14 20:39:53 +0200

    free

---
+++ Formula/free.rb
@@ -0,0 +1,15 @@
+class Free < Formula
+  desc "Shell-based equivalent to the Linux 'free' command for OSX and *BSD in KiB"
+  homepage "https://github.com/bconway/free-for-Mac-OS-X"
+  url "https://raw.githubusercontent.com/bconway/free-for-Mac-OS-X.git"
+  sha256 "8b5ba0ab434141d24900023f79036df394b2d43fb15f93f5b0e1b0e1a6e163c0"
+
+  def install
+    bin.install "free"
+    (bin/"free").chmod 0755
+  end
+
+  test do
+    system "free"
+  end
+end
